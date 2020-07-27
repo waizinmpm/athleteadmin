a1ae1167d23af49770948e34b45df2a5451663c5
Myo Ko Ko <myokoko@management-partners.co.jp>
2020-07-27 14:59:19 +0630

    Scouted list invoice generation phase 1

--- src/components/admin/recruiter/ScoutedList.vue
+++ src/components/admin/recruiter/ScoutedList.vue
@@ -80,7 +80,7 @@
                             <td>{{project.title}}</td>
                             <td>{{project.jobseeker_number}}</td>
                             <td>{{project.jobseeker_name}}</td>
-                            <td>{{project.scout_status}} <span class="btn btn-default">{{$t('common.edit')}}</span> </td>
+                            <td>{{project.scout_status}} <span class="btn btn-default" @click="editStatus(project.id, project.scout_status)">{{$t('common.edit')}}</span> </td>
                             <td style="width:200px;">
                                 <span class="btn btn-default" @click="startChat" v-if="allowChat(project.scout_status)">{{$t('common.chat')}}</span>
                                 <span class="btn btn-default" @click="confirmPayment(project.id, index)" v-if="allowPaymentConfirm(project.scout_status)">{{$t('common.payment_confirm')}}</span>
@@ -119,6 +119,73 @@
                 </pagination>
             </div>
         </div>
+		<div id="myModal" :class="['modal',requireInvoiceForm ? 'modal-open' : 'modal-close' ]">
+			<!-- Modal content -->
+			<div class="modal-content">
+				<span class="close" @click="requireInvoiceForm = false">&times;</span>
+				<div class="container-fluid">
+					<div class="row">
+						<div class="col-sm-6">
+							<h4>仕事</h4>
+							<dl class="row">
+								<dt class="col-sm-2 text-right">管理番号</dt>
+								<dd class="col-sm-9">{{ invoiceForm.management_number }}</dd>
+								<dt class="col-sm-2 text-right">タイトル</dt>
+								<dd class="col-sm-9">{{ invoiceForm.title }}</dd>
+							</dl>
+							<h4>請求先企業会員</h4>
+							<dl class="row">
+								<dt class="col-sm-2 text-right">会員番号</dt>
+								<dd class="col-sm-9">{{ invoiceForm.recruiter_number }}</dd>
+								<dt class="col-sm-2 text-right">企業名</dt>
+								<dd class="col-sm-9">{{ invoiceForm.recruiter_name }}</dd>
+							</dl>
+							<dl class="row">
+								<dt class="col-sm-4">請求書送付先メールアドレス</dt>
+								<dd class="col-sm-8">{{ invoiceForm.email }}</dd>
+							</dl>
+							<h4>仲介手数料</h4>
+							<div class="form-group row">
+								<div class="col-sm-2"></div>
+								<div class="col-sm-6">
+									<input type="text" class="form-control text-right" v-model="invoiceForm.default_amount">
+								</div>
+								<label class="col-sm-1">円</label>
+							</div>
+							<dl class="row">
+								<dt class="col-sm-2 text-right">消費税</dt>
+								<dd class="col-sm-6 text-right">{{ Number(invoiceForm.tax).toLocaleString() }}</dd>
+								<label class="col-sm-1">円</label>
+							</dl>
+							<dl class="row">
+								<dt class="col-sm-2 text-right">請求金額</dt>
+								<dd class="col-sm-6 text-right">{{ Number(invoice_amount).toLocaleString() }}</dd>
+								<label class="col-sm-1">円</label>
+							</dl>
+							<div class="form-group row">
+								<label class="col-sm-2 text-right">備考</label>
+								<div class="col-sm-6">
+									<textarea rows="5" class="form-control"></textarea>
+								</div>
+							</div>
+							<div class="form-group row">
+								<div class="col-sm-9 text-right">
+									
+									<button class="btn btn-primary">請求書プレビュー</button>
+								</div>
+							</div>
+						</div>
+						<div class="col-sm-6">
+							<h4>請求書プレビュー</h4>
+						</div>
+						<div class="col-sm-6">
+							<button class="btn btn-danger" style="margin-right: 1rem;" @click="requireInvoiceForm = false">キャンセル</button>
+						</div>
+					</div>					
+				</div>
+			</div>
+		</div>
+		
     </div>
 </template>
 
@@ -134,12 +201,14 @@ import DataTableServices from "../../DataTable/DataTableServices";
                 sortOrders[column.name] = -1;
             });
             return {
+				requireInvoiceForm: false,
                 base_url: "/v1/admin/scout-list",
                 columns: columns,
                 sortOrders: sortOrders,
                 
                 filteredData:{
-                    recruiter_id: '',
+					recruiter_id: '',
+					title: '',
                     recruiter_name: '',
                     from_date: '',
                     to_date:'',
@@ -163,12 +232,26 @@ import DataTableServices from "../../DataTable/DataTableServices";
                     placeholder: {
                         date: '年 - 月 - 日',
                     }
-                },   
+				},  
+				invoiceForm: {
+					scout_id: 0,
+					management_number: 0,
+					title: 'Sample title',
+					recruiter_number: '',
+					recruiter_name: '',
+					email: '',
+					tax: 20000,
+					default_amount: 200000,
+					invoice_amount: 0,
+					remark: '',
+				} 
             }
         },
         methods: {
 			allowChat(status) {
-				return status == this.$configs.scouts.interested;
+				return status == this.$configs.scouts.interested || 
+					status == this.$configs.scouts.unclaimed ||
+					status == this.$configs.scouts.billed;
 			},
 			allowBilling(status) {
 				return status == this.$configs.scouts.unclaimed;
@@ -179,9 +262,65 @@ import DataTableServices from "../../DataTable/DataTableServices";
 			startChat() {
 				alert("Now will start chatting...");
 			},
+			editStatus(scout_id, scout_status) {
+				this.$swal({
+					allowOutsideClick: false, 
+					title: "Change status",
+					width: 500,
+					text: null,  
+					icon: null,
+					confirmButtonColor: "#ffb700",                       
+					confirmButtonText: "OK",
+					cancelButtonColor: "#0062ff",                       
+					cancelButtonText: "Cancel",
+					customClass: {
+						confirmButton: 'border-style',
+						cancelButton: 'cancelbtn border-style',
+						input: 'd-flex flex-options',
+					},
+					showCloseButton: true,
+					showCancelButton: true,
+					input: 'radio',
+					inputOptions: new Promise((resolve) => {
+						resolve({
+							[this.$configs.scouts.interested]: this.$configs.scouts.interested,
+							[this.$configs.scouts.declined]: this.$configs.scouts.declined,
+							[this.$configs.scouts.expired]: this.$configs.scouts.expired,
+							[this.$configs.scouts.unclaimed]: this.$configs.scouts.unclaimed,
+							[this.$configs.scouts.billed]: this.$configs.scouts.billed,
+							[this.$configs.scouts.payment_confirmed]: this.$configs.scouts.payment_confirmed,
+						})					
+					}),
+					inputValue: scout_status,
+					inputValidator: (value) => {
+						if (!value) {
+							return 'You need to choose something!'
+						}
+					}
+				})
+				.then(r => {
+					if (r.isConfirmed) {
+						// --Todo access api
+
+						let scout = this.$data.projects.data.find(x => {
+							return x.id == scout_id
+						});						
+						scout.scout_status = r.value;
+					}					
+				});
+			},
 			generateBill(scoutId, index) {
-				alert("Bill is successfully generated...");		
-				this.$data.projects.data[index].scout_status = this.$configs.scouts.billed;
+				let scout = this.$data.projects.data[index];
+				this.invoiceForm.scout_id = scout.id;
+				this.invoiceForm.title = scout.title;
+				this.invoiceForm.management_number = scout.management_number;
+				this.invoiceForm.recruiter_number = scout.recruiter_number;
+				this.invoiceForm.recruiter_name = scout.recruiter_name;
+				this.invoiceForm.email = scout.recruiter_email;
+				this.invoiceForm.default_amount = 200000;
+				this.invoiceForm.tax = 20000;
+				this.requireInvoiceForm = true;
+				//this.$data.projects.data[index].scout_status = this.$configs.scouts.billed;
 			},
 			confirmPayment(scoutId, index) {
 				if (confirm("Are you sure?")) {
@@ -201,7 +340,73 @@ import DataTableServices from "../../DataTable/DataTableServices";
         computed: {
             totalScouts: function() {
                 return this.$data.projects.total;
-            }
-		}
+			},
+			invoice_amount() {
+				return Number(this.invoiceForm.default_amount) + Number(this.invoiceForm.tax);
+			}
+		},
+		
     }
 </script>
+<style>
+.flex-options {
+	flex-direction: column;
+	align-items: center;
+	align-content: space-between;
+}
+.flex-options label {
+	flex: 1 1 0px;
+	min-width: 150px;	
+	text-align: left;
+}
+
+/* The Modal (background) */
+.modal {
+  position: fixed; /* Stay in place */
+  z-index: 10; /* Sit on top */
+  left: 0;
+  top: 0;
+  width: 100%; /* Full width */
+  height: 100%; /* Full height */
+  overflow: auto; /* Enable scroll if needed */
+  background-color: rgb(0,0,0); /* Fallback color */
+  background-color: rgba(0,0,0,0.4); /* Black w/ opacity */
+}
+
+.modal-open {
+	display: block;
+}
+
+.modal-close {
+	display: none;
+}
+
+/* Modal Content/Box */
+.modal-content {
+  background-color: #fefefe;
+  margin: 70px auto; /* 15% from the top and centered */
+  padding: 20px;
+  border: 1px solid #888;
+  width: 80%;
+}
+
+/* The Close Button */
+.close {
+  color: #aaa;
+  float: right;
+  font-size: 28px;
+  font-weight: bold;
+}
+
+.close:hover,
+.close:focus {
+  color: black;
+  text-decoration: none;
+  cursor: pointer;
+}
+textarea {
+	resize: vertical;
+}
+
+
+</style>
