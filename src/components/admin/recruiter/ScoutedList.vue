2a344da8f76cb3028185c4f28c48ed09a31afd9d
Chhuani <chhuani@management-partners.co.jp>
2020-08-18 15:32:09 +0630

    CN-ScoutedList Conflict resolve

--- src/components/admin/recruiter/ScoutedList.vue
+++ src/components/admin/recruiter/ScoutedList.vue
@@ -580,16 +580,13 @@ export default {
     opacity: 0;
     transform: scale(0);
 }
-<<<<<<< HEAD
-
-=======
 [contenteditable] {
   outline: 0px solid transparent;
 }
 .flex-options {
 	flex-direction: column;
 	align-items: center;
-	align-content: space-between;
+	align-content: space-between; 
 }
 .flex-options label {
 	flex: 1 1 0px;
@@ -616,7 +613,6 @@ export default {
   background-color: rgb(0,0,0); /* Fallback color */
   background-color: rgba(0,0,0,0.4); /* Black w/ opacity */
 }
->>>>>>> 9243a17494cf014ab9944549a19530cc64ac0469
 
 .modal-open {
 	display: block;
