cc4f60d81adc0edf5b05b29e48739709e1edd206
zayarpn <zayarpn@management-partners.co.jp>
2020-09-01 11:32:15 +0630

    add label of columns for sorting

--- src/lang/en/job-list.js
+++ src/lang/en/job-list.js
@@ -1,13 +1,13 @@
 export default {
     "columns": [
-        {"label":"Recruiter Id"},
-        {"label":"Recruiter Name"},
-        {"label":"Job Number"},
-        {"label":"Job Title"},
-        {"label":"Number of applicants"},
-        {"label":"Scout trustees"},
-        {"label":"Post period"},
-        {"label":"Status"},
-        {"label":""},
+        {"label":"recruiter_number", "name": "Recruiter Id"},
+        {"label":"recruiter_name", "name": "Recruiter Name"},
+        {"label":"job_number", "name": "Job Number"},
+        {"label":"title", "name": "Job Title"},
+        {"label":"jobapplied_count", "name": "Number of applicants"},
+        {"label":"scout_count", "name": "Scout trustees"},
+        {"label":"job_post_date", "name": "Post period"},
+        {"label":"record_status", "name": "Status"},
+        {"label":"", "name": ""},
 	],
 }
--- src/lang/jp/job-list.js
+++ src/lang/jp/job-list.js
@@ -1,13 +1,13 @@
 export default {
     "columns": [
-        {"label":"企業番号"},
-        {"label":"企業名"},
-        {"label":"求人番号"},
-        {"label":"求人タイトル"},
-        {"label":"求⼈応募者数"},
-        {"label":"スカウト受託者数"},
-        {"label":"掲載期間"},
-        {"label":"ステータス"},
-        {"label":""},
+        {"label":"recruiter_number", "name": "企業番号"},
+        {"label":"recruiter_name", "name": "企業名"},
+        {"label":"job_number", "name": "求人番号"},
+        {"label":"title", "name": "求人タイトル"},
+        {"label":"jobapplied_count", "name": "求⼈応募者数"},
+        {"label":"scout_count", "name": "スカウト受託者数"},
+        {"label":"job_post_date", "name": "掲載期間"},
+        {"label":"record_status", "name": "ステータス"},
+        {"label":"", "name": ""},
 	],
 };
