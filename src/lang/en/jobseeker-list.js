35dfdae3bfeb8972e60f0f8f2024b8a6113a5ed1
zayarpn <zayarpn@management-partners.co.jp>
2020-09-02 13:50:15 +0630

    sorting data by table columns in jobseeker list of admin panel

--- src/lang/en/jobseeker-list.js
+++ src/lang/en/jobseeker-list.js
@@ -2,9 +2,9 @@ export default {
     "jobseeker_member_list": "Job Seeker Member Information List",
     "search_jobseeker_placeholder": "Please enter the job seeker member number and job seeker name",
     "columns": [
-        {"label":"Job Seeker Number"},
-        {"label":"Job Seeker Name"},
-        {"label":"Status"},
-        {"label":""},
+        {"label":"jobseeker_number", "name": "Job Seeker Number"},
+        {"label":"jobseeker_name", "name": "Job Seeker Name"},
+        {"label":"record_status", "name": "Status"},
+        {"label":"", "name": ""},
 	],
 }
--- src/lang/jp/jobseeker-list.js
+++ src/lang/jp/jobseeker-list.js
@@ -2,9 +2,9 @@ export default {
     "jobseeker_member_list": "求職者会員情報一覧",
     "search_jobseeker_placeholder": "求職者会員番号、求職者名を⼊⼒してください……",
     "columns": [
-        {"label":"求職者会員番号"},
-        {"label":"求職者名"},
-        {"label":"ステータス"},
-        {"label":""},
+        {"label":"jobseeker_number", "name": "求職者会員番号"},
+        {"label":"jobseeker_name", "name": "求職者名"},
+        {"label":"record_status", "name": "ステータス"},
+        {"label":"", "name": ""},
 	],
 }
