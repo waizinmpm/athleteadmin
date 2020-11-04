d6da9f3f390051289ec3153f877d6ef25783a56f
EiM0h <eimohmohaung.ei@gmail.com>
2020-11-04 14:44:39 +0630

    admin home card edit

--- src/components/admin/AdminHome.vue
+++ src/components/admin/AdminHome.vue
@@ -8,22 +8,28 @@
    <div class="row col-xs-12">
         <div class="col-md-3">
             <div class="dashboard-card">
-                <h5>{{count.recruiter}}</h5>
-                <p>企業会員</p>
+                <div class="txt-warp">
+                    <h5>{{count.recruiter}}</h5>
+                    <p>企業会員</p>
+                </div>
                 <span class="custom-icon"><i class="fa fa-user color-one" style="font-size:40px;"></i></span>
             </div>  
         </div>   
          <div class="col-md-3">
             <div class="dashboard-card">
-                <h5>{{count.jobseeker}}</h5>
-                <p>求職者会員</p>
+                <div class="txt-warp">
+                    <h5>{{count.jobseeker}}</h5>
+                    <p>求職者会員</p>
+                </div>
                 <span class="custom-icon"><i class="fa fa-files-o color-three" style="font-size:40px;"></i></span>
             </div>             
         </div> 
         <div class="col-md-3">
             <div class="dashboard-card">
-                <h5>{{count.job}}</h5>
-                <p>求人</p>
+                <div class="txt-warp">
+                    <h5>{{count.job}}</h5>
+                    <p>求人</p>
+                </div>                
                 <span class="custom-icon"><i class="fa fa-suitcase color-five" style="font-size:40px;"></i></span>
             </div>             
         </div>                    
@@ -31,22 +37,28 @@
     <div class="row col-xs-12">
          <div class="col-md-3">
             <div class="dashboard-card">
-                <h5>{{count.jobApply}}</h5>
-                <p>求人問合わせ/応募者一覧</p>
+                <div class="txt-warp">
+                    <h5>{{count.jobApply}}</h5>
+                    <p>求人問合わせ/<br class="br">応募者一覧</p>
+                    </div>
                 <span class="custom-icon"><i class="fa fa-file-text color-four" style="font-size:40px;"></i></span>
             </div>             
         </div> 
         <div class="col-md-3">
             <div class="dashboard-card">
-                <h5>{{count.scout}}</h5>
-                <p>内定未請求</p>
+                <div class="txt-warp">
+                    <h5>{{count.scout}}</h5>
+                    <p>内定未請求</p>
+                </div>
                <span class="custom-icon"><i class="fa fa-users color-two" style="font-size:40px;"></i></span>
             </div>  
         </div>   
         <div class="col-md-3">
             <div class="dashboard-card">
-                <h5>{{count.payment_count}}</h5>
-                <p>請求済</p>
+                <div class="txt-warp">
+                    <h5>{{count.payment_count}}</h5>
+                    <p>請求済</p>
+                </div>
                 <span class="custom-icon"><i class="fa fa-credit-card color-six" style="font-size:40px;"></i></span>
             </div>             
         </div>     
@@ -79,6 +91,8 @@
     height: 100vh;
 }
 .dashboard-card{
+    display: flex;
+    align-items: center;
     -webkit-border-radius: 4px;
     -moz-border-radius: 4px;
     border-radius: 4px;        
@@ -115,8 +129,24 @@
     color:#F8A220;
 }
 .custom-icon{
-    position: absolute;
-    top: 60px;
-    right: 80px;
+    margin-left: auto;
+}
+.br {
+    display: none;
+}
+
+@media (max-width:1440px) {
+    .br {
+        display: block;
+    }
+    .dashboard-card {
+        min-height: 170px;
+        padding: 20px;
+    }
+}
+@media (max-width:1280px) {
+    .col-md-3{
+        width: 30%;
+    }
 }
 </style>
