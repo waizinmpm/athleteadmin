20f8daeb068c0feb1e594ab17118396a0fd27bea
zayarpn7 <zayarpn@management-partners.co.jp>
2020-11-11 13:46:26 +0630

    add more columns in detail

--- src/components/admin/recruiter/JobDetail.vue
+++ src/components/admin/recruiter/JobDetail.vue
@@ -2,64 +2,74 @@
     <div class="job-detail row">
         <h5 class="main-header">{{ recruiter_job.job_number }} {{ recruiter_job.title }}</h5>
         <dl class="detail-list">
-            <dt class="detail-head">募集ポジション</dt>
-            <dd class="detail-data">{{ recruiter_job.incharge_name }}</dd>
+            <dt class="detail-head">求人タイトル</dt>
+            <dd class="detail-data">{{ recruiter_job.title }}</dd>
+            <dt class="detail-head">職種</dt>
+            <dd class="detail-data">{{ recruiter_job.occupation_description }}</dd>
             <dt class="detail-head">雇用形態</dt>
             <dd class="detail-data">
-                <div v-for="(result, index) in employment_types" :key="result.id">
+                <div v-for="result in employment_types" :key="result.id">
                     <input type="checkbox" v-bind:value="result.employment_type_name" v-bind:id="result.employment_type_name" class="custom-control-input custom-checkbox"              v-model="recruiter_job.employment_types" disabled />
-                    <label
+                    <label :for="result.employment_type_name" class="custom-control-label custom-checkbox-label">{{result.employment_type_name}}</label>
+                    <!-- <label
                     :for="result.employment_type_name"
                     class="custom-control-label custom-checkbox-label"
-                    >{{$t('jobcreate.employment_type['+ index+']')}}</label>
+                    >{{$t('jobcreate.employment_type['+ index+']')}}</label> -->
                 </div>
             </dd>
+            <dt class="detail-head">仕事内容</dt>
+            <dd class="detail-data"><pre>{{ recruiter_job.job_description }}</pre></dd>
+            <dt class="detail-head">応募資格・語学力</dt>
+            <dd class="detail-data"><pre>{{ recruiter_job.qualification }}</pre></dd>
+            <dt class="detail-head">給与・待遇・ビザサポート</dt>
+            <dd class="detail-data"><pre>{{ recruiter_job.allowance }}</pre></dd>
+            <dt class="detail-head">勤務開始日</dt>
+            <dd class="detail-data">{{ recruiter_job.job_start_date }}</dd>
             <dt class="detail-head">勤務地</dt>
             <dd class="detail-data"><pre>{{ recruiter_job.job_location }}</pre></dd>
-            <dt class="detail-head">仕事の内容</dt>
+            <dt class="detail-head">応募先アドレス</dt>
+            <dd class="detail-data"><pre>{{ recruiter_job.application_address }}</pre></dd>
+            <dt class="detail-head">求職者へのメッセージ</dt>
+            <dd class="detail-data"><pre>{{ recruiter_job.message }}</pre></dd>
+            <dt class="detail-head">エリア (検索用キーワード)</dt>
+            <dd class="detail-data">
+                <span v-for="country in countries" :key="country.id">
+                    {{ recruiter_job.country_id == country.id ? country.country_name : ''}}
+                </span>
+            </dd>
+            <dt class="detail-head">職種(検索用キーワード)</dt>
+            <dd class="detail-data">
+                <span v-for="occupation in occupations" :key="occupation.id">
+                    {{ recruiter_job.occupation_id == occupation.id ? occupation.occupation_name : ''}}
+                </span>
+            </dd>
+            <dt class="detail-head">その他キーワード (求人情報に表示されます)</dt>
+            <dd class="detail-data">
+                <div class="col-md-4" v-for="(keyword, index) in other_keywords" :key="index">
+                    <input
+                    type="checkbox"
+                    :value="keyword"
+                    :id="keyword"
+                    class="custom-control-input custom-checkbox"
+                    v-model="recruiter_job.other_keywords"
+                    disabled
+                    />
+                    <label
+                    :for="keyword"
+                    class="custom-control-label custom-checkbox-label"
+                    >{{ $t('jobcreate.other_keywords['+ index+']') }}</label>
+                </div>
+            </dd>
+            <dt class="detail-head">更新日</dt>
+            <dd class="detail-data">{{ recruiter_job.job_post_date }}</dd>
+            <!-- <dt class="detail-head">仕事の内容</dt>
             <dd class="detail-data"><pre>{{ recruiter_job.job_description }}</pre></dd>
             <dt class="detail-head">応募資格</dt>
             <dd class="detail-data"><pre>{{ recruiter_job.qualification }}</pre></dd>
             <dt class="detail-head">給与・待遇</dt>
-            <dd class="detail-data"><pre>{{ recruiter_job.allowance }}</pre></dd>
+            <dd class="detail-data"><pre>{{ recruiter_job.allowance }}</pre></dd> -->
         </dl>
-        <span @click="$router.go(-1)" class="btn btn-back float-right">一覧へ戻る</span> 
-        <!-- <router-link to="$router.go(-1)" class="btn btn-back float-right">
-           一覧へ戻る
-        </router-link> -->
-        <!-- 
-            <div class="row">
-                <div class="col-md-4">募集ポジション(記入例)チーフディレクター</div>
-                <div class="col-md-8">{{ recruiter_job.incharge_name }}</div>
-            </div>
-            <div class="row">
-            <div class="col-md-4">雇用形態(記入例)正社員</div>
-            <div class="col-md-8">
-                <div v-for="(result, index) in employment_types" :key="result.id">
-                    <input type="checkbox" v-bind:value="result.employment_type_name" v-bind:id="result.employment_type_name" class="custom-control-input custom-checkbox"              v-model="recruiter_job.employment_types" disabled />
-                    <label
-                    :for="result.employment_type_name"
-                    class="custom-control-label custom-checkbox-label"
-                    >{{$t('jobcreate.employment_type['+ index+']')}}</label>
-                </div>
-            </div>
-        </div>
-        <div class="row">
-            <div class="col-md-4">勤務地(記入例)ジャカルタ</div>
-            <div class="col-md-8">{{ recruiter_job.job_location }}</div>
-        </div>
-        <div class="row">
-            <div class="col-md-4">仕事の内容</div>
-            <div class="col-md-8">{{ recruiter_job.job_description }}</div>
-        </div>
-        <div class="row">
-            <div class="col-md-4">応募資格</div>
-            <div class="col-md-8">{{ recruiter_job.qualification }}</div>
-        </div>
-        <div class="row">
-            <div class="col-md-4">給与・待遇・ビザサポート</div>
-            <div class="col-md-8">{{ recruiter_job.allowance }}</div>
-        </div> -->
+        <span @click="$router.go(-1)" class="btn btn-back float-right">一覧へ戻る</span>
     </div>
 </template>
 
@@ -67,15 +77,23 @@
 export default {
     data() {
         return{
-            recruiter_job: {
-                employment_types: []
-            },
-            employment_types: []
+            recruiter_job: {},
+            employment_types: [],
+            occupations: [],
+            countries: [],
+            other_keywords: [
+                "新卒歓迎",
+                "未経験歓迎",
+                "シニア歓迎",
+                "上場企業",
+            ],
         }
     },
 
     created() {
-        let loading = this.$loading.show();
+        
+        this.getJobInfo();
+        /* // let loading = this.$loading.show();
         this.$api.get("/v1/recruiter/jobs/" + `${this.$route.params.id}` + "/edit")
         .then((res) => {
             this.recruiter_job = res.data.data;
@@ -83,9 +101,30 @@ export default {
             this.$api.get("/v1/recruiter/getalldata")
             .then((response) => {
                 this.employment_types = response.data.employment;
-                loading.hide();
+                // loading.hide();
             })
-        });
+        }); */
+    },
+
+    methods : {
+        async getJobInfo() {
+            let loading = this.$loading.show();
+            try {
+                const response = await Promise.all([
+                    this.$api.get("/v1/recruiter/jobs/" + `${this.$route.params.id}` + "/edit"),
+                    this.$api.get("/v1/recruiter/getalldata")
+                ]);
+                this.recruiter_job      = response[0].data.data;
+                this.employment_types   = response[1].data.employment;
+                this.occupations        = response[1].data.occupation;
+                this.countries          = response[1].data.country;
+                this.recruiter_job.employment_types = this.recruiter_job.employment_types.split(",");
+                this.recruiter_job.other_keywords   = this.recruiter_job.other_keywords.split(",");
+            } catch (error) {
+                console.log(error);
+            }
+            loading.hide();
+        }
     }
     
 }
