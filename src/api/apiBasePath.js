b9c30491a6f0c5ab7422a937c7142445b5ed8aac
zayarpn7 <zayarpn@management-partners.co.jp>
2021-01-15 09:22:53 +0630

    fixed bugs of token expired auto logout

--- src/api/apiBasePath.js
+++ src/api/apiBasePath.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 import store from '../store';
 import router from '../router';
 import alertService from '../services/AlertService';
-let errorShowing = false;
+// let errorShowing = false;
 const api = axios.create({
     baseURL: process.env.VUE_APP_API_URL, 
     // headers: {'Authorization': `Bearer ${store.getters.token}`}
@@ -24,16 +24,16 @@ api.interceptors.response.use(function (response) {
     // response error
     console.log('api response.status', error.response.status);
 	if (400 === error.response.status) 
-	{	
-		if (!errorShowing) {
-            errorShowing = true;
+	{
+		// if (!errorShowing) {
+        //     errorShowing = true;
             // let redirect = '/';
             // localStorage.removeItem('adminuser');
             // window.location = redirect;
-            store.commit('logout');
-            router.push({ path: "/" });
-			alertService.showInfoDialog(null,'長時間操作がなかったため、ログアウトしました。')
-		}
+        store.commit('logout');
+        router.push({ path: "/" });
+        alertService.showInfoDialog(null,'長時間操作がなかったため、ログアウトしました。')
+		// }
 		
 	}
 	return Promise.reject(error);
