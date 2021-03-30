e94b0fbbf030e6f10315385916270c1f1541cabc
waizinmpm <waizin@management-partners.co.jp>
2021-03-30 09:12:11 +0630

    not found page

--- src/components/NotFoundPage.vue
+++ src/components/NotFoundPage.vue
@@ -1,26 +1,49 @@
 <template>
-	<div class="page-wrap d-flex flex-row align-items-center">
-		<div class="container">
-			<div class="row justify-content-center">
-				<div class="col-md-12 text-center">
-					<h2>404</h2>
-					<div class="mb-4 lead">The page you are looking for was not found.</div>
-					<router-link to="/">Back to Home</router-link>					
-				</div>
-			</div>
-		</div>
-	</div>
+    <div class="page-wrap d-flex flex-row align-items-center">
+    <div class="container">
+        <div class="row justify-content-center">
+            <div class="col-md-12 text-center">
+                <span class="display-1 d-block txt-404">404</span>
+                <div class="mb-4 lead">Page Not Found</div>
+                <a href="/" class="btn btn-link">Back to Home</a>
+            </div>
+        </div>
+    </div>
+</div>
+
 </template>
 <script>
 export default {
-	
+	metaInfo: {
+		title: 'アスリート リボーン プロジェクト',
+	},
 }
-</script>
+</script> 
 <style scoped>
 .page-wrap {
-    background: #dedede;
-}
-.page-wrap {
     min-height: 100vh;
+}
+.txt-404{
+    font-size: 15em;
+}
+.lead{
+    font-size: 3rem;
+    color:#7b7b7b;
+}
+.btn-link{
+    width: 150px;
+    border-radius: 25px !important;
+    background:#0062ff;
+}
+.btn-link:hover{
+    text-decoration: none;
+}
+@media screen and (max-width: 767px){
+    .txt-404{
+        font-size: 12em;
+    }
+    .lead{
+        font-size: 2em;
+    }
 }
 </style>
